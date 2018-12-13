2edeb2ef6412d41436d6e805411f4fcb928d0349
Clement coxam <clement.coxam@gmail.com>
2018-12-13 19:11:15 +0100

    First Commit
    Add Views

---
+++ BatailleNavale/UI/Controller/PlayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlayViewController.swift
+//  BatailleNavale
+//
+//  Created by formation6 on 13/12/2018.
+//  Copyright © 2018 diginamic. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    @IBAction func returnOnTap(_ sender: Any) {
+        self.dismiss(animated: true, completion: ({
+            
+        }))
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
